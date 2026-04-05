af2e42c5c16d4e656e7c0dc207ecc60532e9c404
maksimpopulov <viktorpopulov@gmail.com>
2026-04-05 15:36:42 +0300

    Update fun.cpp

--- src/fun.cpp
+++ src/fun.cpp
@@ -2,13 +2,103 @@
 #include "fun.h"
 
 unsigned int faStr1(const char *str) {
-    return 0;
+    if (str == nullptr) return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool hasDigit = false;
+    while (*str) {
+        if (std::isspace(*str)) {
+            if (inWord && !hasDigit) {
+                count++;
+            }
+            inWord = false;
+            hasDigit = false;
+        } else {
+            if (!inWord) {
+                inWord = true;
+                hasDigit = false;
+            }
+            if (std::isdigit(*str)) {
+                hasDigit = true;
+            }
+        }
+        str++;
+    }
+    if (inWord && !hasDigit) {
+        count++;
+    }
+    return count;
 }
 
 unsigned int faStr2(const char *str) {
-    return 0;
+    if (str == nullptr) return 0;
+    unsigned int count = 0;
+    bool inWord = false;
+    bool validWord = true;
+    int charIndex = 0;
+    while (*str) {
+        if (std::isspace(*str)) {
+            if (inWord && validWord) {
+                count++;
+            }
+            inWord = false;
+            validWord = true;
+            charIndex = 0;
+        } else {
+            if (!inWord) {
+                inWord = true;
+                validWord = true;
+                charIndex = 0;
+            }
+            if (validWord) {
+                if (charIndex == 0) {
+                    if (!(*str >= 'A' && *str <= 'Z')) {
+                        validWord = false;
+                    }
+                } else {
+                    if (!(*str >= 'a' && *str <= 'z')) {
+                        validWord = false;
+                    }
+                }
+            }
+            charIndex++;
+        }
+        str++;
+    }
+    if (inWord && validWord) {
+        count++;
+    }
+    return count;
 }
 
 unsigned int faStr3(const char *str) {
-    return 0;
+    if (str == nullptr) return 0;
+    unsigned int totalLength = 0;
+    unsigned int wordCount = 0;
+    unsigned int currentLength = 0;
+    bool inWord = false;
+    while (*str) {
+        if (std::isspace(*str)) {
+            if (inWord) {
+                totalLength += currentLength;
+                wordCount++;
+                currentLength = 0;
+                inWord = false;
+            }
+        } else {
+            if (!inWord) {
+                inWord = true;
+                currentLength = 0;
+            }
+            currentLength++;
+        }
+        str++;
+    }
+    if (inWord) {
+        totalLength += currentLength;
+        wordCount++;
+    }
+    if (wordCount == 0) return 0;
+    double average = static_cast<double>(totalLength) / wordCount;
+    return static_cast<unsigned int>(std::round(average));
 }
