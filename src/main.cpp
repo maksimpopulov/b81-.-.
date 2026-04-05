c126edfdac9b75d07660a11859aaa76ea2ea25c1
maksimpopulov <viktorpopulov@gmail.com>
2026-04-05 15:45:48 +0300

    Update main.cpp

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,12 @@
 // Copyright 2022 UNN-IASR
+#include "iosream"
 #include "fun.h"
 
 int main() {
+    char* myStr = "sdf1 fsdf5sdfs gslll adfvs";
+    std::cout << str << "\n";
+    std::cout << "функция 1: " << faStr1(str) << "\n";
+    std::cout << "функция 2: " << faStr2(str) << "\n";
+    std::cout << "функция 3: " << faStr3(str) << "\n";
     return 0;
 }
